49f74c1ff82fba7cfa7c3675b732cb8e3502b574
adamdubicki <adam@freshworks.io>
2018-03-03 16:31:02 -0800

    added length 1 error check

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func respond(res http.ResponseWriter, obj interface{}) {
@@ -43,15 +44,23 @@ func handleMove(res http.ResponseWriter, req *http.Request) {
 	var tailResult Point
 	var optimalResult string
 
-	// start := time.Now()
+	start := time.Now()
 	// bm.GameBoard.show()
 	go func() {
 		foodMove := Point{-1, -1}
 		foodResult := bm.findBestFood()
 		if foodResult.Food.X != -1 {
 			foodPath := shortestPath(bm.OurHead, foodResult.Food, bm.GameBoard)
-			if len(foodPath) >= 2 && pathIsSafe(foodPath, bm.Req.You, bm.GameBoard) {
+
+			var pathIsSafeCheck bool
+			if bm.Req.You.Length <= 2 {
+				pathIsSafeCheck = true
+			} else {
+				pathIsSafeCheck = pathIsSafe(foodPath, bm.Req.You, bm.GameBoard)
 				foodPath = reverseList(foodPath)
+			}
+
+			if len(foodPath) >= 2 && pathIsSafeCheck {
 				foodMove = foodPath[1]
 			}
 		}
@@ -63,7 +72,7 @@ func handleMove(res http.ResponseWriter, req *http.Request) {
 		copy := bm.GameBoard.copy()
 		copy.insert(bm.Req.You.Tail(), food())
 		tailPath := shortestPath(bm.OurHead, bm.Req.You.Tail(), copy)
-		if len(tailPath) >= 2 {
+		if len(tailPath) >= 2 && bm.Req.You.Length > 2 {
 			if len(tailPath) == 2 && bm.Req.You.Health > 99 {
 				tailMove = Point{-1, -1}
 			} else {
@@ -81,20 +90,24 @@ func handleMove(res http.ResponseWriter, req *http.Request) {
 
 	go func() {
 		root := createRoot(data)
-		optimalChannel <- root.getOptimalMove()
+		currentMove := NO_MOVE
+		if bm.Req.You.Length >= 2 {
+			currentMove = root.getOptimalMove()
+		}
+		optimalChannel <- currentMove
 	}()
 
 	// fmt.Println("---------------------")
 	for i := 0; i < 3; i++ {
 		select {
 		case foodResult = <-foodChannel:
-			// fmt.Println("Food Result:", getDirection(bm.Req.You.Head(), foodResult), time.Since(start))
+			fmt.Println("Food Result:", getDirection(bm.Req.You.Head(), foodResult), time.Since(start))
 			continue
 		case tailResult = <-tailChannel:
-			// fmt.Println("Tail Result:", getDirection(bm.Req.You.Head(), tailResult), time.Since(start))
+			fmt.Println("Tail Result:", getDirection(bm.Req.You.Head(), tailResult), time.Since(start))
 			continue
 		case optimalResult = <-optimalChannel:
-			// fmt.Println("Optimal Result:", optimalResult, time.Since(start))
+			fmt.Println("Optimal Result:", optimalResult, time.Since(start))
 			continue
 		}
 	}
@@ -106,11 +119,11 @@ func handleMove(res http.ResponseWriter, req *http.Request) {
 	} else if bm.GameBoard.tileInBounds(tailResult) && bm.Req.Turn > 3 {
 		currentMove = getDirection(bm.Req.You.Head(), tailResult)
 	} else if optimalResult != NO_MOVE {
-		// fmt.Println("WENT FOR OPTIMAL")
 		currentMove = optimalResult
 	} else {
 		neighbours := bm.GameBoard.getValidTiles(bm.Req.You.Head())
 		if len(neighbours) > 0 {
+			fmt.Println(neighbours)
 			currentMove = getDirection(bm.Req.You.Head(), neighbours[0])
 		} else {
 			currentMove = UP
